72c468b0ad3ec09c2ef6124b5067496f92d79f93
neurosutras <neurosutras@gmail.com>
2017-12-14 15:26:02 -0800

    121417: Merge with Grace

--- test-ring/tstsubworld.py
+++ test-ring/tstsubworld.py
@@ -1,23 +1,42 @@
+from mpi4py import MPI
 from neuron import h
-pc = h.ParallelContext()
-pc.subworlds(1)
+import click
 
-s = 'world ({0}, {1}) bbs ({2}, {3}) net ({4}, {5})'.format(
-    pc.id_world(), pc.nhost_world(), pc.id_bbs(), pc.nhost_bbs(), pc.id(), pc.nhost()
-)
 
-print(s)
+@click.command()
+@click.option("--subworlds", type=int, default=1)
+def main(subworlds):
+    """
 
-from ring import runring
+    :param subworlds: int
+    :return:
+    """
+    pc = h.ParallelContext()
+    pc.subworlds(subworlds)
 
-pc.runworker()
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+    size = comm.size
 
-for ncell in range(5, 10):
-  pc.submit(runring, ncell, 1, 100)
+    print 'rank: ({6}, {7}, world: ({0}, {1}) bbs ({2}, {3}) net ({4}, {5})'.format(
+        pc.id_world(), pc.nhost_world(), pc.id_bbs(), pc.nhost_bbs(), pc.id(), pc.nhost()
+    )
 
-while (pc.working()):
-  print(pc.pyret())  
+    (s)
 
-pc.done()
-h.quit()
+    from ring import runring
 
+    pc.runworker()
+
+    for ncell in range(5, 10):
+      pc.submit(runring, ncell, 1, 100)
+
+    while (pc.working()):
+      print(pc.pyret())
+
+    pc.done()
+    h.quit()
+
+
+if __name__ == '__main__':
+    main()
