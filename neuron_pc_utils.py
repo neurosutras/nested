9812813d84cee042aefb3810a2de67de930fb45f
Aaron D. Milstein <neurosutras@gmail.com>
2017-12-22 20:11:23 -0800

    122217: parallel_optimize renamed to optimize Re-factoring optimize to support: 1) choice of framework:  a) ipyparallel  b) mpi4py.futures  c) neuron.h.ParallelContext 2) choice of running module via  a) -m nested.optimize  b) from nested.optimize import *; then configuring framework and param_gen interactively

--- neuron_pc_utils.py
+++ neuron_pc_utils.py
@@ -4,7 +4,7 @@ import click
 import uuid
 
 
-script_filename = 'init_neuron.py'
+script_filename = 'neuron_pc_utils.py'
 
 context = Context()
 context.global_comm = MPI.COMM_WORLD
@@ -12,11 +12,11 @@ context.global_comm = MPI.COMM_WORLD
 
 def initialize_subworlds(subworld_size=1):
     """
-    neuron.h.ParallelContext provides a bulletin-board style interface for concurrent processing, while also allowing
-    collective communication within groups of processes (subworlds). While MPI is used for communication,
-    ParallelContext does not expose the underlying MPI communicators for each subworld. This method initializes this
-    parallelization scheme, and places both a split ParallelContext and a split MPI.COMM_WORLD into the global Context
-    contained within each MPI rank.
+    neuron.h.ParallelContext provides a bulletin-board-style interface for concurrent processing, while also allowing
+    collective communication within groups of processes (subworlds). While ParallelContext uses MPI for communication
+    between processes, it does not expose the underlying MPI communicators for each subworld. This method initializes a
+    global Context on each MPI rank to make both a split ParallelContext and a split MPI.COMM_WORLD accessible for
+    nested parallel operations.
     :param subworld_size: int
     """
     # requires a global variable context: :class:'Context'
@@ -34,6 +34,8 @@ def initialize_subworlds(subworld_size=1):
     context.comm = context.global_comm.Create(sub_group)
     context.subworld_id = context.comm.bcast(int(context.pc.id_bbs()), root=0)
     context.num_subworlds = context.comm.bcast(int(context.pc.nhost_bbs()), root=0)
+    # dictionary acts as a temporary storage container on the master process for results retrieved from the
+    # ParallelContext bulletin board.
     context.collected = {}
     assert context.rank == context.comm.rank and context.global_rank == context.global_comm.rank and \
            context.global_comm.size / context.subworld_size == context.num_subworlds, 'pc.ids do not match MPI ranks'
@@ -41,7 +43,8 @@ def initialize_subworlds(subworld_size=1):
 
 def _pc_apply(func, key, *args):
     """
-    After applying the specified function, keep each subworld from returning until all subworlds have applied as well.
+    Execute the specified function within a ParallelContext subworld. Block each subworld from returning until all
+    subworlds have executed the function.
     :param func: callable
     :param key: string
     :param args: list
@@ -51,19 +54,20 @@ def _pc_apply(func, key, *args):
         context.pc.take(key)
         count = context.pc.upkscalar()
         context.pc.post(key, count + 1)
-        while context.pc.look(key) and context.pc.upkscalar() < context.num_subworlds:
-            pass
-    return
+        while True:
+            if context.pc.look(key) and context.pc.upkscalar() == context.num_subworlds:
+                return
 
 
 def pc_apply(func, *args):
     """
-    neuron.h.ParallelContext lacks a native method to apply a function to all ranks of all subworlds. This method makes
-    use of a Context that contains a ParallelContext and an MPI.COMM_WORLD to implement an apply operation.
+    ParallelContext lacks a native method to guarantee execution of a function within all subworlds. This method
+    implements a synchronous (blocking) apply operation.
     :param func: callable
     :param args: list
     """
-    key = str(uuid.uuid4())
+    # key = str(uuid.uuid4())
+    key = 'apply_'+func.__name__
     context.pc.post(key, 0)
     for i in xrange(context.num_subworlds):
         context.pc.submit(_pc_apply, func, key, *args)
@@ -74,24 +78,38 @@ def pc_apply(func, *args):
 
 def pc_get_results(keys=None):
     """
-    This method clears the neuron.h.ParallelContext bulletin board, and returns a dictionary of all results indexed by
-    their submission key. If a list of keys are provided, pc_get_results returns only those requested results, leaving
-    the rest of the cleared results in the dict 'collected' in the global Context.
+    If no keys are specified, this method is a blocking operation that waits until all previously submitted jobs have
+    been completed, retrieves all results from the bulletin board, and stores them in the 'collected' dict in the global
+    Context on the master process, indexed by their submission key.
+    If a list of keys is provided, pc_get_results first checks if the results have already been placed in the
+    'collected' dict, and otherwise blocks until all requested results are available. Results retrieved from the
+    bulletin board that were not requested are left in the 'collected' dict in the global Context.
     :param keys: list
     :return: dict
     """
-    while context.pc.working():
-        key = context.pc.userid()
-        context.collected[key] = context.pc.pyret()
     if keys is None:
+        while context.pc.working():
+            key = context.pc.userid()
+            context.collected[key] = context.pc.pyret()
         keys = context.collected.keys()
-    return {key: context.collected.pop(key) for key in keys if key in context.collected}
+        return {key: context.collected.pop(key) for key in keys}
+    else:
+        pending_keys = [key for key in keys if key not in context.collected]
+        while pending_keys:
+            if context.pc.working():
+                key = context.pc.userid()
+                context.collected[key] = context.pc.pyret()
+                if key in pending_keys:
+                    pending_keys.pop(key)
+            else:
+                break
+        return {key: context.collected.pop(key) for key in keys if key in context.collected}
 
 
 class PCAsyncResult(object):
     """
-    Makes use of the dict 'collected' in the global Context to provide a non-blocking interface for collecting results
-    from unfinished jobs.
+    Makes use of the 'collected' dict in the global Context on the master process to provide a non-blocking interface
+    for collecting results from unfinished jobs.
     """
     def __init__(self, keys):
         """
@@ -109,6 +127,8 @@ class PCAsyncResult(object):
         if context.pc.working():
             key = context.pc.userid()
             context.collected[key] = context.pc.pyret()
+        else:
+            return True
         if all(key in context.collected for key in self.keys):
             return True
         else:
@@ -123,13 +143,16 @@ class PCAsyncResult(object):
         if not self.ready():
             return None
         else:
-            return [context.collected.pop(key) for key in self.keys]
+            try:
+                return [context.collected.pop(key) for key in self.keys]
+            except KeyError:
+                raise KeyError('PCAsyncResult: all jobs have completed, but not all requested keys were found')
 
 
 def pc_map_sync(func, *sequences):
     """
-    neuron.h.ParallelContext lacks a native method to map a sequence of parameters to a function, using all available
-    subworlds. This method makes use of a Context that contains a ParallelContext and an MPI.COMM_WORLD to implement a
+    ParallelContext lacks a native method to apply a function to sequences of arguments, using all available
+    processes, and returning the results in the same order as the specified sequence. This method implements a
     synchronous (blocking) map operation. Returns results as a list in the same order as the specified sequences.
     :param func: callable
     :param sequences: list
@@ -149,10 +172,10 @@ def pc_map_sync(func, *sequences):
 
 def pc_map_async(func, *sequences):
     """
-    neuron.h.ParallelContext lacks a native method to map a sequence of parameters to a function, using all available
-    subworlds. This method makes use of a Context that contains a ParallelContext and an MPI.COMM_WORLD to implement an
-    asynchronous (blocking) map operation. When ready(), get() returns results as a list in the same order as the
-    specified sequences.
+    ParallelContext lacks a native method to apply a function to sequences of arguments, using all available
+    processes, and returning the results in the same order as the specified sequence. This method implements an
+    asynchronous (non-blocking) map operation. Returns a PCAsyncResult object to track progress of the submitted jobs.
+    When ready(), get() returns results as a list in the same order as the specified sequences.
     :param func: callable
     :param sequences: list
     :return: list
@@ -205,12 +228,29 @@ def main(subworld_size):
     context.pc.runworker()
     pc_apply(set_count)
     pc_apply(set_count, 5)
+
     results1 = pc_map_sync(collect_ranks, range(10))
     pprint.pprint(results1)
+
     results2 = pc_map_async(collect_ranks, range(10, 20))
     results3 = pc_map_async(collect_ranks, range(20, 30))
     start_time = time.time()
+
     print 'collected result keys: %s' % str(context.collected.keys())
+    while not results2.ready():
+        pass
+    results2 = results2.get()
+    pprint.pprint(results2)
+    print 'collected result keys: %s' % str(context.collected.keys())
+    results4 = pc_get_results()
+    pprint.pprint(results4)
+    print 'collected result keys: %s' % str(context.collected.keys())
+    while not results3.ready():
+        pass
+    results3 = results3.get()
+    pprint.pprint(results3)
+    print 'collected result keys: %s' % str(context.collected.keys())
+    """
     while not (results2.ready() and results3.ready()):
         pass
     print 'Could have been working on rank %i! Elapsed time: %.3f' % (context.global_rank, time.time() - start_time)
@@ -221,7 +261,9 @@ def main(subworld_size):
     results3 = results3.get()
     pprint.pprint(results3)
     print 'collected result keys: %s' % str(context.collected.keys())
+    """
     context.pc.done()
+    h.quit()
 
 
 if __name__ == '__main__':
