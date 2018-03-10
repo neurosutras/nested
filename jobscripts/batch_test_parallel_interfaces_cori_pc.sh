7fc53623163e6a93b6380578f2bcabf79b62bfb4
Aaron D. Milstein <neurosutras@gmail.com>
2018-03-09 22:27:39 -0800

    20180309: nested.parallel.ParallelContextInterface:  - apply and get now rely on neuron.h.pc.master_works_on_jobs()

--- jobscripts/batch_test_parallel_interfaces_cori_pc.sh
+++ jobscripts/batch_test_parallel_interfaces_cori_pc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
-#SBATCH -J test_parallel_interfaces_pc_20180224
-#SBATCH -o /global/homes/a/aaronmil/python_modules/nested/logs/test_parallel_interfaces_pc_20180224.%j.o
-#SBATCH -e /global/homes/a/aaronmil/python_modules/nested/logs/test_parallel_interfaces_pc_20180224.%j.e
+#SBATCH -J test_parallel_interfaces_pc_20180309
+#SBATCH -o /global/homes/a/aaronmil/python_modules/nested/logs/test_parallel_interfaces_pc_20180309.%j.o
+#SBATCH -e /global/homes/a/aaronmil/python_modules/nested/logs/test_parallel_interfaces_pc_20180309.%j.e
 #SBATCH -q debug
-#SBATCH -N 6
+#SBATCH -N 16
 #SBATCH -L SCRATCH
 #SBATCH -C haswell
 #SBATCH -t 0:30:00
@@ -16,4 +16,4 @@ set -x
 cd $HOME/python_modules/nested
 export DATE=$(date +%Y%m%d_%H%M%S)
 
-srun -N 6 -n 192 -c 2 python test_parallel_interfaces.py --framework=pc
+srun -N 16 -n 256 -c 2 python test_parallel_interfaces.py --framework=pc --procs-per-worker=2
