3bdffc3a8d785903362df247a2e77fdd58f46337
Aaron D. Milstein <neurosutras@gmail.com>
2018-08-10 15:21:58 -0700

    20180810: master branch:  - Fixed error in population_annealing_example

--- population_annealing_example.py
+++ population_annealing_example.py
@@ -35,6 +35,8 @@ def get_objectives(features):
     """
     objectives = {}
     for feature_name in ['f1', 'g', 'h']:
+        if feature_name not in features:
+            return dict(), dict()
         objective_name = feature_name
         objectives[objective_name] = features[feature_name]
     f2 = features['g'] * features['h']
@@ -74,16 +76,16 @@ def main(num_params, pop_size, wrap_bounds, max_iter, path_length, hot_start, st
     feature_names = ['g', 'h']
     objective_names = ['f1', 'f2', 'g', 'h']
     get_features = complex_problem
-    """
+
     if storage_file_path is None:
         storage_file_path = 'data/%s_pop_anneal_example_storage.hdf5' % \
                             (datetime.datetime.today().strftime('%Y%m%d_%H%M'))
-    """
+    
     pop_anneal = PopulationAnnealing(param_names=param_names, feature_names=feature_names,
                                      objective_names=objective_names, pop_size=pop_size, x0=x0, bounds=bounds,
                                      wrap_bounds=wrap_bounds, seed=0, max_iter=max_iter, path_length=path_length,
                                      adaptive_step_factor=0.9, survival_rate=0.20, disp=True, hot_start=hot_start,
-                                     storage_file_path=storage_file_path, select='select_survivors_by_rank_and_fitness')
+                                     storage_file_path=storage_file_path)  #, select='select_survivors_by_rank_and_fitness')
 
     features = [{} for pop_id in xrange(pop_size)]
     objectives = [{} for pop_id in xrange(pop_size)]
