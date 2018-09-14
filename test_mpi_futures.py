45b96d9736e07ec677018e6891bfec44eba99d16
Aaron D. Milstein <neurosutras@gmail.com>
2018-09-14 13:17:06 -0700

    20180914:  - Troubleshooting mpi4py.futures on Cori.

--- test_mpi_futures.py
+++ test_mpi_futures.py
@@ -13,34 +13,33 @@ print 'Rank: %i sees size: %i before executor is built. (%s)' % (rank, size, __n
 
 
 def do_work(i):
-    if rank == 0:
+    if rank == 1:
         # Master process executes code below
-        open_ranks = range(1, size)
+        open_ranks = range(2, size)
         for worker_rank in open_ranks:
             future = comm.irecv(source=worker_rank)
             val = future.wait()
             # print rank, worker_rank, val
         for worker_rank in open_ranks:
-            comm.isend(0, dest=worker_rank)
+            comm.isend(1, dest=worker_rank)
     else:
         # Worker processes execute code below
         # print("I am a worker with rank {}.".format(rank))
-        comm.isend(rank, dest=0)
-        future = comm.irecv(source=0)
+        comm.isend(rank, dest=1)
+        future = comm.irecv(source=1)
         val = future.wait()
-        return rank
+    return rank
 
 
 def main():
     num_workers = size - 1
-    num_tasks = 0
+    executor = MPIPoolExecutor()
     for i in xrange(3):
         start_time = time.time()
-        tasks = range(num_tasks, num_tasks + num_workers)
-        executor = MPIPoolExecutor()
+        tasks = range(num_workers)
         future_list = executor.map(do_work, tasks)
         returned_ranks = []
-        do_work(None)
+        # do_work(None)
         for result in future_list:
             returned_ranks.append(result)
         used_workers = len(set(returned_ranks))
