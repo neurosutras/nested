58cdffec86fc3e83079847e5d7bc54888224428e
Aaron D. Milstein <neurosutras@gmail.com>
2020-02-13 13:04:52 -0800

    20200213: feature-analyze:  - Separating nested.analyze from nested.optimize  - Enforcing that a model_id be provided to each worker during feature and objective computations.

--- analyze.py
+++ analyze.py
@@ -28,9 +28,9 @@ ipcluster start -n N &
 python -m nested.analyze --config-file-path=$PATH_TO_CONFIG_YAML --framework=ipyp --param-file-path=$PATH_TO_PARAM_YAML
 """
 __author__ = 'Aaron D. Milstein, Grace Ng, and Prannath Moolchand'
+from nested.optimize_utils import *
 from nested.parallel import *
 from nested.optimize import evaluate_population
-from nested.optimize_utils import *
 import click
 
 try:
@@ -99,21 +99,16 @@ def main(cli, config_file_path, sobol_analysis, storage_file_path, param_file_pa
             if len(context.model_id) < 1 and len(context.model_key) < 1:
                 param_arrays = [context.x0_array]
                 model_ids = [0]
-                model_labels = [None]
+                model_labels = [[]]
             else:
-                # TODO: get_analyze_model_group
-                # param_arrays, model_ids, model_labels = \
-                #    get_analyze_model_group(param_file_path=context.param_file_path,
-                #                            storage_file_path=context.storage_file_path, model_id=context.model_id,
-                #                            model_key=context.model_key)
-                print('nested.analyze: not implemented yet')
-                sys.stdout.flush()
-                return
-            print(type(param_arrays), len(param_arrays), type(model_ids), len(model_ids))
-            sys.stdout.flush()
-            features, objectives = evaluate_population(param_arrays, model_ids=model_ids, export=context.export)
+                param_arrays, model_ids, model_labels = \
+                    get_model_group(context.param_names, context.objective_names,
+                                    param_file_path=context.param_file_path,
+                                    storage_file_path=context.storage_file_path, model_id=context.model_id,
+                                    model_key=context.model_key, verbose=context.disp)
+            features, objectives = evaluate_population(context, param_arrays, model_ids, context.export)
             if context.export:
-                merge_exported_data(param_arrays, model_ids, model_labels, features, objectives,
+                merge_exported_data(context, param_arrays, model_ids, model_labels, features, objectives,
                                     export_file_path=context.export_file_path, output_dir=context.output_dir,
                                     verbose=context.disp)
             for shutdown_func in context.shutdown_worker_funcs:
@@ -144,196 +139,74 @@ def main(cli, config_file_path, sobol_analysis, storage_file_path, param_file_pa
         raise e
 
 
-def optimize():
+def get_model_group(param_names, objective_names, param_file_path=None, storage_file_path=None, model_id=None,
+                    model_key=None, verbose=False):
     """
 
+    :param param_names: list of str
+    :param objective_names: list of str
+    :param param_file_path: str (path)
+    :param storage_file_path: str (path)
+    :param model_id: list of int or str
+    :param model_key: list of str
+    :param verbose: bool
+    :return: tuple of lists
     """
-    for generation in context.param_gen_instance():
-        features, objectives = evaluate_population(generation)
-        context.param_gen_instance.update_population(features, objectives)
-        del features
-        del objectives
-    for shutdown_func in context.shutdown_worker_funcs:
-        context.interface.apply(shutdown_func)
+    if param_file_path is None and storage_file_path is None:
+        raise RuntimeError('nested.analyze: either a param_file_path or a storage_file_path must be provided to'
+                           'analyze models specified by model_id or model_key')
 
+    requested_param_arrays = []  # list of array
+    requested_model_ids = []  # list of int
+    requested_model_labels = []  # list of lists of str
 
-def evaluate_population(population, export=False):
-    """
-    The instructions for computing features and objectives specified in the config_file_path are now followed for each
-    individual member of a population of parameter arrays (models). If any compute_features or filter_feature function
-    returns an empty dict, or a dict that contains the key 'failed', that member of the population is completely removed
-    from any further computation. This frees resources for remaining individuals. If any dictionary of features or
-    objectives does not contain the full set of expected items, the param_gen_instance will mark those models as failed
-    when update_population is called.
-    :param population: list of arr
-    :param export: bool; whether to export model intermediates
-    :return: tuple of list of dict
-    """
-    params_pop_dict = dict(enumerate(population))
-    pop_ids = list(range(len(population)))
-    features_pop_dict = {pop_id: dict() for pop_id in pop_ids}
-    objectives_pop_dict = {pop_id: dict() for pop_id in pop_ids}
-    for stage in context.stages:
-        if not pop_ids:
-            if context.disp:
-                print('nested.optimize: all models failed to compute required features')
-                sys.stdout.flush()
-            break
-        params_pop_list = [params_pop_dict[pop_id] for pop_id in pop_ids]
-        if 'args' in stage:
-            group_size = len(stage['args'][0])
-            args_population = [stage['args'] for pop_id in pop_ids]
-        elif 'get_args_static_func' in stage:
-            stage['args'] = context.interface.execute(stage['get_args_static_func'])
-            group_size = len(stage['args'][0])
-            args_population = [stage['args'] for pop_id in pop_ids]
-        elif 'get_args_dynamic_func' in stage:
-            features_pop_list = [features_pop_dict[pop_id] for pop_id in pop_ids]
-            args_population = context.interface.map_sync(stage['get_args_dynamic_func'], params_pop_list,
-                                                         features_pop_list)
-            group_size = len(args_population[0][0])
-        else:
-            args_population = [[] for pop_id in pop_ids]
-            group_size = 1
-        if 'shared_features' in stage:
-            for pop_id in pop_ids:
-                features_pop_dict[pop_id].update(stage['shared_features'])
-        elif 'compute_features_shared_func' in stage:
-            args = args_population[0]
-            this_x = params_pop_list[0]
-            sequences = [[this_x] * group_size] + args + [[export] * group_size]
-            results_list = context.interface.map_sync(stage['compute_features_shared_func'], *sequences)
-            if 'filter_features_func' in stage:
-                this_shared_features = \
-                    context.interface.execute(stage['filter_features_func'], results_list, {}, export)
+    if param_file_path is not None:
+        if not os.path.isfile(param_file_path):
+            raise Exception('nested.analyze: invalid param_file_path: %s' % param_file_path)
+        if model_key is None or len(model_key) < 1:
+            raise RuntimeError('nested.analyze: missing required parameter: a model_key must be provided to to analyze '
+                               'models specified by a param_file_path: %s' % param_file_path)
+        model_param_dict = read_from_yaml(param_file_path)
+        for i, this_model_key in enumerate(model_key):
+            requested_model_ids.append(i)
+            if str(this_model_key) in model_param_dict:
+                requested_param_arrays.append(param_dict_to_array(model_param_dict[str(this_model_key)], param_names))
+                requested_model_labels.append([str(this_model_key)])
+            elif str(this_model_key).isnumeric() and int(this_model_key) in model_param_dict:
+                requested_param_arrays.append(param_dict_to_array(model_param_dict[int(this_model_key)], param_names))
+                requested_model_labels.append([str(this_model_key)])
             else:
-                this_shared_features = dict()
-                for features_dict in results_list:
-                    this_shared_features.update(features_dict)
-            if not this_shared_features or 'failed' in this_shared_features:
-                raise RuntimeError('nested.optimize: compute_features_shared function: %s failed' %
-                                   stage['compute_features_shared'])
-            stage['shared_features'] = this_shared_features
-            for pop_id in pop_ids:
-                features_pop_dict[pop_id].update(stage['shared_features'])
-            del this_shared_features
-        else:
-            pending = []
-            for this_x, args in zip(params_pop_list, args_population):
-                sequences = [[this_x] * group_size] + args + [[export] * group_size]
-                pending.append(context.interface.map_async(stage['compute_features_func'], *sequences))
-            while not all(result.ready(wait=0.1) for result in pending):
-                time.sleep(0.1)
-            primitives = [result.get() for result in pending]
-            del pending
-            if 'filter_features_func' in stage:
-                features_pop_list = [features_pop_dict[pop_id] for pop_id in pop_ids]
-                new_features = context.interface.map_sync(stage['filter_features_func'], primitives, features_pop_list,
-                                                          [export] * len(pop_ids))
-                del features_pop_list
-                for pop_id, this_features in zip(pop_ids, new_features):
-                    if not this_features:
-                        this_features = {'failed': True}
-                    features_pop_dict[pop_id].update(this_features)
-                del new_features
-            else:
-                for pop_id, results_list in zip(pop_ids, primitives):
-                    this_features = \
-                        {key: value for features_dict in results_list for key, value in viewitems(features_dict)}
-                    if not this_features:
-                        this_features = {'failed': True}
-                    features_pop_dict[pop_id].update(this_features)
-            del primitives
-            temp_pop_ids = list(pop_ids)
-            for pop_id in temp_pop_ids:
-                if not features_pop_dict[pop_id] or 'failed' in features_pop_dict[pop_id]:
-                    pop_ids.remove(pop_id)
-            del temp_pop_ids
-        if 'synchronize_func' in stage:
-            context.interface.apply(stage['synchronize_func'])
-    for get_objectives_func in context.get_objectives_funcs:
-        temp_pop_ids = list(pop_ids)
-        features_pop_list = [features_pop_dict[pop_id] for pop_id in pop_ids]
-        primitives = context.interface.map_sync(get_objectives_func, features_pop_list, [export] * len(pop_ids))
-        del features_pop_list
-        for pop_id, this_result in zip(temp_pop_ids, primitives):
-            if this_result is None:
-                pop_ids.remove(pop_id)
-            else:
-                this_features, this_objectives = this_result
-                features_pop_dict[pop_id].update(this_features)
-                objectives_pop_dict[pop_id].update(this_objectives)
-        del primitives
-        del temp_pop_ids
-    sys.stdout.flush()
-    features_pop_list = [features_pop_dict[pop_id] for pop_id in range(len(population))]
-    objectives_pop_list = [objectives_pop_dict[pop_id] for pop_id in range(len(population))]
-    for reset_func in context.reset_worker_funcs:
-        context.interface.apply(reset_func)
-    return features_pop_list, objectives_pop_list
-
-
-def export_intermediates(x, export_file_path=None, discard=True):
-    """
-    During calculation of features and objectives, source methods may respond to the export flag by appending
-    intermediates like simulation output to separate .hdf5 files on each process. This method evaluates a single
-    parameter array and merges the resulting .hdf5 files.
-    :param x: array
-    :param export_file_path: str
-    :param discard: bool
-    """
-    if export_file_path is not None:
-        context.export_file_path = export_file_path
+                raise RuntimeError('nested.analyze: provided model_key: %s not found in param_file_path: %s' %
+                                   (str(this_model_key), param_file_path))
     else:
-        export_file_path = context.export_file_path
-    start_time = time.time()
-    """
-    TODO: Need a method to label and save model intermediates without having to duplicate model execution at the end of
-    optimization.
-    temp_output_path_list = [temp_output_path for temp_output_path in
-                             context.interface.get('context.temp_output_path') if os.path.isfile(temp_output_path)]
-    for temp_output_path in temp_output_path_list:
-        os.remove(temp_output_path)
-    """
-    features, objectives = evaluate_population([x], export=True)
-    if context.disp:
-        print('nested.optimize: export_intermediates: evaluating individual took %.2f s' % (time.time() - start_time))
-    start_time = time.time()
-    temp_output_path_list = [temp_output_path for temp_output_path in
-                             context.interface.get('context.temp_output_path') if os.path.isfile(temp_output_path)]
-    if not temp_output_path_list:
-        if context.disp:
-            print('nested.optimize: export_intermediates: no data exported - no temp_output_data files found')
-    else:
-        if context.comm.rank == 0:
-            temp_output_path_list = list(set(temp_output_path_list))  # remove duplicates
-            merge_exported_data(temp_output_path_list, export_file_path, verbose=False)
-            if discard:
-                for temp_output_path in temp_output_path_list:
-                    os.remove(temp_output_path)
-            if context.disp:
-                print('nested.optimize: exporting output to %s took %.2f s' % (export_file_path, time.time() - start_time))
-    sys.stdout.flush()
+        if not os.path.isfile(storage_file_path):
+            raise Exception('nested.analyze: invalid storage_file_path: %s' % storage_file_path)
+        if model_id is not None and len(model_id) > 0:
+            with h5py.File(context.storage_file_path, 'r') as f:
+                count = 0
+                for group in f.values():
+                    if 'count' in group.attrs:
+                        count = max(count, group.attrs['count'])
+            for this_model_id in model_id:
+                if int(this_model_id) >= count:
+                    raise RuntimeError('nested.analyze: invalid model_id: %i' % int(this_model_id))
+                requested_model_ids.append(int(this_model_id))
+                # TODO: find requested param_arrays and any associated labels in file
+        else:
+            if model_key is None or len(model_key) < 1:
+                model_key = ('best',)
+                if verbose:
+                    print('nested.analyze: no model_id or model_key specified; defaulting to evaluate best model in '
+                          'storage_file_path: %s' % storage_file_path)
+                    sys.stdout.flush()
+            valid_model_keys = set(objective_names)
+            valid_model_keys.add('best')
+            for this_model_key in model_key:
+                if str(this_model_key) not in valid_model_keys:
+                    raise RuntimeError('nested.analyze: invalid model_key: %s' % str(this_model_key))
+                # TODO: find requested param_arrays, model_ids and any additional associated labels in file
 
-    if not (all([feature_name in features[0] for feature_name in context.feature_names]) and
-            all([objective_name in objectives[0] for objective_name in context.objective_names])):
-        if context.disp:
-            print('nested.optimize: export_intermediates: model failed')
-        return features[0], objectives[0], export_file_path
-    exported_features = {key: features[0][key] for key in context.feature_names}
-    exported_objectives = {key: objectives[0][key] for key in context.objective_names}
-    
-    if os.path.exists(export_file_path):
-        f = h5py.File(export_file_path, 'r+')
-        f.attrs['param_names'] = np.array(context.param_names, dtype='S32')
-        f.attrs['feature_names'] = np.array(context.feature_names, dtype='S32')
-        for par, val in zip(context.param_names, x):    
-            f.attrs['par_{!s}'.format(par)] = val
-        for k, v in exported_features.items():
-            f.attrs['fea_{!s}'.format(k)] = v
-        f.close()
-
-    return exported_features, exported_objectives, export_file_path
+    return requested_param_arrays, requested_model_ids, requested_model_labels
 
 
 if __name__ == '__main__':
